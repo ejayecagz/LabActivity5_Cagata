a99449be286be03180905dee41aba42679f8c2c2
cagata <carrotcagzzz@gmail.com>
2022-12-18 01:39:50 +0800

    Add SimpleCalc main function

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,12 +1,38 @@
 package SimpleCalc;
 
-import javax.swing.*;
+import LeapYear.LeapYearGUI;
 
-public class SimpleCalcGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleCalcGUI extends JFrame {
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
     private JButton btnCompute;
     private JTextField tfNumber2;
     private JTextField lblResult;
+
+
+    public static void main(String[] args) {
+        JFrame app = new SimpleCalcGUI("Simple Calculator");
+        app.setTitle("Simple Calculator");
+        app.setVisible(true);
+        app.setLocationRelativeTo(null);
+    }
+    public SimpleCalcGUI(String title) {
+        super(title);
+        this.setContentPane(panel1);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.pack();
+        lblResult.setEditable(false);
+
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }

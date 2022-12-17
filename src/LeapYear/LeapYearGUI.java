8bfddce13380b27038ae7d3b12578033086e7551
cagata <carrotcagzzz@gmail.com>
2022-12-18 01:28:05 +0800

    Complete LeapYear

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -13,7 +13,15 @@ public class LeapYearGUI extends JFrame {
         btnCheckYear.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                int year = 0;
+                year = Integer.parseInt(tfYear.getText());
+                if (year % 400 == 0) {
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                } else if (year % 100 != 0 && year % 4 == 0) {
+                    JOptionPane.showMessageDialog(null, "Leap year");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Not a leap year");
+                }
             }
         });
     }
@@ -21,7 +29,7 @@ public class LeapYearGUI extends JFrame {
     public static void main(String[] args) {
         LeapYearGUI app = new LeapYearGUI("Leap Year Checker");
         app.setContentPane(app.panel1);
-        app.setSize(800, 800);
+        app.setSize(500, 500);
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
         app.setVisible(true);
     }
